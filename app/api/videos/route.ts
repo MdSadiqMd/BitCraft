3b4a74e3748c55c8af6b0a33312dc8287f0329bb
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-18 16:18:13 +0530

    Videos get Url added

---
+++ app/api/videos/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { StatusCodes } from "http-status-codes";
+
+const prisma = new PrismaClient();
+
+/**
+ * Handles GET request to /api/video
+ * Retrieves all videos ordered by createdAt in descending order
+ * @param request NextRequest object
+ * @returns NextResponse with status 200 and the videos in JSON format
+ * If there's an error, returns NextResponse with status 500 and the error message in JSON format
+ */
+export async function GET() {
+    try {
+        const videos = await prisma.video.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return NextResponse.json({
+            videos
+        }, StatusCodes.OK as any);
+    } catch (error) {
+        console.log("Error in Retriving Videos: ", error);
+        return NextResponse.json({
+            message: "Error in Retriving Videos: " + error
+        }, StatusCodes.INTERNAL_SERVER_ERROR as any);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
