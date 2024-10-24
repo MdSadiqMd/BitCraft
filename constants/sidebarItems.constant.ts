bad19679a97c3dcdfcaf33194d140be69689cc85
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-24 14:54:37 +0530

    Feat: Added sidebar and navbar

---
+++ constants/sidebarItems.constant.ts
@@ -0,0 +1,11 @@
+import {
+    LayoutDashboardIcon,
+    Share2Icon,
+    UploadIcon,
+} from "lucide-react";
+
+export const sidebarItems = [
+    { href: "/home", icon: LayoutDashboardIcon, label: "Home Page" },
+    { href: "/social-share", icon: Share2Icon, label: "Social Share" },
+    { href: "/video-upload", icon: UploadIcon, label: "Video Upload" },
+];
